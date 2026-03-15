c0df9d57a8bc99212a1f1872c0a6afbccf39a7c8
Angel Figueroa <angsanchez241205@gmail.com>
2026-03-15 15:38:14 -0600

    Cambios realizados: - Le agregue el .swift a unos archivos que me aparecia le hacian falta - Se crearon y modificaron los archivos para el CamScanner.
    Nota: Para este Sprint 1 se creo el CamScanner de una forma simulada, se agregara la deteccion de objetos funcional cuando tengamos acceso por medio de API a la database para poder traer el texto informativo de los objetos directamente de la database. Al igual el boton de audio/escuchar por el momento simplemente es preview, la funcionalidad final para el audio generativo se estima quede concluida para el ultimo sprint. Queda pendiente el trabajo de mejora de la interfaz

---
+++ H3-CoqueGuide/H3-CoqueGuide/CamScannerView.swift
@@ -0,0 +1,7 @@
+//
+//  CamScanner.swift
+//  
+//
+//  Created by Angel De Jesus Sanchez Figueroa on 15/03/26.
+//
+
---
+++ H3-CoqueGuide/H3-CoqueGuide/CameraService.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  
+//
+//  Created by Angel De Jesus Sanchez Figueroa on 15/03/26.
+//
+
