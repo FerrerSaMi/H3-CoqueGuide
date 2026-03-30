7248d0a797f995b9ffe3fab880b7907a5f0b4d1a
FerrerSaMi <sproleshalcon7@gmail.com>
2026-03-30 17:29:15 -0600

    Refactor AISurveyTests
    Se actualizo a la nueva encuesta las tests

--- H3-CoqueGuide/H3-CoqueGuideTests/AISurveyTests.swift
+++ H3-CoqueGuide/H3-CoqueGuideTests/AISurveyTests.swift
@@ -3,34 +3,62 @@
 //  H3-CoqueGuideTests
 //
 
-/*
+
 import XCTest
 @testable import H3_CoqueGuide
 
 @MainActor
-final class AISurveyTests: XCTestCase {
+final class AISurveyTest: XCTestCase {
 
-    func testSurveyViewModelInitialState() {
+    func testInitialStateIsCorrect() {
         let viewModel = SurveyViewModel()
 
-        XCTAssertEqual(viewModel.name, "")
-        XCTAssertEqual(viewModel.ageText, "")
-        XCTAssertTrue(viewModel.selectedPreferences.isEmpty)
-        XCTAssertEqual(viewModel.availableTime, "")
-        XCTAssertEqual(viewModel.specificSearch, "")
+        XCTAssertEqual(viewModel.currentScreen, .home)
+        XCTAssertEqual(viewModel.currentStepIndex, 0)
+
+        XCTAssertEqual(viewModel.gender, "")
+        XCTAssertEqual(viewModel.ageRange, "")
+        XCTAssertEqual(viewModel.plannedTime, "")
+        XCTAssertEqual(viewModel.attractionPreference, "")
+        XCTAssertEqual(viewModel.resolvedAttractionPreference, "")
+        XCTAssertEqual(viewModel.specificAttraction, "")
         XCTAssertEqual(viewModel.preferredLanguage, "Español")
         XCTAssertEqual(viewModel.selectedCoquePersonality, "Neutral")
+
+        XCTAssertEqual(viewModel.aiDescription, "")
+        XCTAssertFalse(viewModel.isLoading)
+        XCTAssertNil(viewModel.errorMessage)
     }
 
-    func testTogglePreferenceAddsAndRemovesPreference() {
+    func testStartSurveyResetsAnswersAndMovesToQuestionScreen() {
         let viewModel = SurveyViewModel()
-        let preference = "Ver"
 
-        viewModel.togglePreference(preference)
-        XCTAssertTrue(viewModel.selectedPreferences.contains(preference))
+        viewModel.gender = "Mujer"
+        viewModel.ageRange = "19 - 29"
+        viewModel.plannedTime = "1 - 2 horas"
+        viewModel.attractionPreference = "Shows"
+        viewModel.resolvedAttractionPreference = "Shows"
+        viewModel.specificAttraction = "Mirador"
+        viewModel.preferredLanguage = "English"
+        viewModel.selectedCoquePersonality = "Divertido"
+        viewModel.currentStepIndex = 4
+        viewModel.currentScreen = .description
+        viewModel.errorMessage = "Error previo"
 
-        viewModel.togglePreference(preference)
-        XCTAssertFalse(viewModel.selectedPreferences.contains(preference))
+        viewModel.startSurvey()
+
+        XCTAssertEqual(viewModel.currentScreen, .question)
+        XCTAssertEqual(viewModel.currentStepIndex, 0)
+
+        XCTAssertEqual(viewModel.gender, "")
+        XCTAssertEqual(viewModel.ageRange, "")
+        XCTAssertEqual(viewModel.plannedTime, "")
+        XCTAssertEqual(viewModel.attractionPreference, "")
+        XCTAssertEqual(viewModel.resolvedAttractionPreference, "")
+        XCTAssertEqual(viewModel.specificAttraction, "")
+        XCTAssertEqual(viewModel.preferredLanguage, "Español")
+        XCTAssertEqual(viewModel.selectedCoquePersonality, "Neutral")
+        XCTAssertNil(viewModel.errorMessage)
     }
 }
-*/
+
